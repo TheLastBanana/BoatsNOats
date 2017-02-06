601ebd4971e8016f0b998d5358924f41eb3fcd6b
Elliot Colp <thelastbanana@gmail.com>
2017-02-05 21:23:51 -0700

    Add player walk/jump/land sounds

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+// Custom version of the Platformer2DUserControl script
+[RequireComponent(typeof(PlayerController))]
+public class PlayerController : MonoBehaviour
+{
+    private PlayerMovement m_Character;
+    private bool m_Jump;
+
+
+    private void Awake()
+    {
+        m_Character = GetComponent<PlayerMovement>();
+    }
+
+
+    private void Update()
+    {
+        if (!m_Jump)
+        {
+            // Read the jump input in Update so button presses aren't missed.
+            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        m_Character.Move(h, crouch, m_Jump);
+        m_Jump = false;
+    }
+}
---
+++ Assets/PlayerSound.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSound : MonoBehaviour
+{
+    public AudioSource[] stepSounds;
+    public AudioSource[] jumpSounds;
+    public AudioSource[] landSounds;
+    public float landingSoundAirTime = 0.5f;
+
+    private Animator anim;              // Animator component
+    private float prevFootstep;         // Previous footstep value
+    private float lastGroundedTime;     // Last time at which this was grounded
+    private bool wasGrounded;           // Was this grounded last update?
+
+    public void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void Update()
+    {
+        float footstep = anim.GetFloat("Footstep");
+
+        // If footstep oscillates from low to high, play the sound
+        // This allows us to blend animations and not repeat the sound
+        if (prevFootstep < 0 && footstep > 0)
+        {
+            PlayFootstepSound();
+        }
+
+        prevFootstep = footstep;
+        
+        bool grounded = anim.GetBool("Ground");
+        if (grounded)
+        {
+            // Switched from grounded to not grounded and stayed in the air long enough
+            if (!wasGrounded && Time.time - lastGroundedTime > landingSoundAirTime)
+            {
+                PlayLandSound();
+            }
+
+            lastGroundedTime = Time.time;
+        }
+
+        wasGrounded = grounded;
+    }
+    
+    // Play a random footstep sound
+    public void PlayFootstepSound()
+    {
+        PlayRandomSound(stepSounds);
+    }
+
+    // Play the jump sound
+    public void PlayJumpSound()
+    {
+        PlayRandomSound(jumpSounds);
+    }
+
+    // Play the landing sound
+    public void PlayLandSound()
+    {
+        PlayRandomSound(landSounds);
+    }
+
+    // Play a random sound from an array
+    private void PlayRandomSound(AudioSource[] array)
+    {
+        array[Random.Range(0, array.Length)].Play();
+    }
+}
