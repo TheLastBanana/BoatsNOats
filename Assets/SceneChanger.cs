12ff802de8ee4dc6912928ae5df308c8dfcbfce0
Stuart Bildfell <bildfells760@gmail.com>
2017-02-06 21:06:38 -0700

    Basic instant scene change

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityStandardAssets._2D
+{
+    public class SceneChanger : MonoBehaviour
+    {
+        public int nextScene;
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag == "Player")
+            {
+                SceneManager.LoadScene(nextScene);
+            }
+        }
+    }
+}
