a7bb3851201c8e2e8072cffefd99e0fc40e54f00
Braedy Kuzma <braedy@ualberta.ca>
2017-02-07 23:19:49 -0700

    Added getter for vector3 for convenience.

--- Assets/WorldOffsets.cs
+++ Assets/WorldOffsets.cs
@@ -4,4 +4,9 @@ using UnityEngine;
 
 public class WorldOffsets : MonoBehaviour {
     public int x, y; // Offset to alt world
+
+    public Vector3 vec3
+    {
+        get { return new Vector3(x, y, 0); }
+    }
 }
