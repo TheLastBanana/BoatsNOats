afe90e663f8b656cccf8e31d043fb16b2e6d1686
Stuart Bildfell <bildfells760@gmail.com>
2017-02-10 11:59:05 -0700

    Reset button, press 'R' to reset in any level

--- Assets/SceneChanger.cs
+++ Assets/SceneChanger.cs
@@ -8,7 +8,25 @@ namespace UnityStandardAssets._2D
 {
     public class SceneChanger : MonoBehaviour
     {
-        public int nextScene;
+        private int currentScene;
+        private int nextScene;
+
+        // Use this for initialization
+        void Start()
+        {
+            // Make sure scenes are ordered properly in build settings
+            currentScene = SceneManager.GetActiveScene().buildIndex;
+            nextScene = currentScene + 1;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(currentScene);
+            }
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
