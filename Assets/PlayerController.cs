c1ff29f96eaa20eb773686b5fa4429a83af85100
Stuart Bildfell <bildfells760@gmail.com>
2017-02-13 20:05:49 -0700

    Added new player controller
    - Animations and sounds aren't in yet, but those should be simple
    - Player is not frozen when dragging portal like they were before
    - Can't physics interact with objects

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,38 +1,124 @@
-﻿using System;
-using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
+﻿using UnityEngine;
+using System.Collections;
+using Prime31;
 
-// Custom version of the Platformer2DUserControl script
-[RequireComponent(typeof(PlayerController))]
+
 public class PlayerController : MonoBehaviour
 {
-    private PlayerMovement m_Character;
-    private bool m_Jump;
+	// movement config
+	public float gravity = -25f;
+	public float runSpeed = 8f;
+	public float groundDamping = 20f; // how fast do we change direction? higher means faster
+	public float inAirDamping = 5f;
+	public float jumpHeight = 3f;
+
+	[HideInInspector]
+	private float normalizedHorizontalSpeed = 0;
+
+	private CharacterController2D _controller;
+	private Animator _animator;
+    private PlayerSound _sound;
+	private RaycastHit2D _lastControllerColliderHit;
+	private Vector3 _velocity;
 
 
-    private void Awake()
-    {
-        m_Character = GetComponent<PlayerMovement>();
-    }
+	void Awake()
+	{
+        _sound = GetComponent<PlayerSound>();
+		_animator = GetComponent<Animator>();
+		_controller = GetComponent<CharacterController2D>();
+
+		// listen to some events for illustration purposes
+		_controller.onControllerCollidedEvent += onControllerCollider;
+		_controller.onTriggerEnterEvent += onTriggerEnterEvent;
+		_controller.onTriggerExitEvent += onTriggerExitEvent;
+	}
 
 
-    private void Update()
-    {
-        if (!m_Jump)
-        {
-            // Read the jump input in Update so button presses aren't missed.
-            m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
-        }
-    }
+	#region Event Listeners
+
+	void onControllerCollider( RaycastHit2D hit )
+	{
+		// bail out on plain old ground hits cause they arent very interesting
+		if( hit.normal.y == 1f )
+			return;
+
+		// logs any collider hits if uncommented. it gets noisy so it is commented out for the demo
+		//Debug.Log( "flags: " + _controller.collisionState + ", hit.normal: " + hit.normal );
+	}
 
 
-    private void FixedUpdate()
-    {
-        // Read the inputs.
-        bool crouch = Input.GetKey(KeyCode.LeftControl);
-        float h = CrossPlatformInputManager.GetAxis("Horizontal");
-        // Pass all parameters to the character control script.
-        m_Character.Move(h, crouch, m_Jump);
-        m_Jump = false;
-    }
+	void onTriggerEnterEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerEnterEvent: " + col.gameObject.name );
+	}
+
+
+	void onTriggerExitEvent( Collider2D col )
+	{
+		Debug.Log( "onTriggerExitEvent: " + col.gameObject.name );
+	}
+
+	#endregion
+
+
+	// the Update loop contains a very simple example of moving the character around and controlling the animation
+	void Update()
+	{
+		if( _controller.isGrounded )
+			_velocity.y = 0;
+
+		{
+			normalizedHorizontalSpeed = 1;
+			if( transform.localScale.x < 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Run" ) );
+		}
+		{
+			normalizedHorizontalSpeed = -1;
+			if( transform.localScale.x > 0f )
+				transform.localScale = new Vector3( -transform.localScale.x, transform.localScale.y, transform.localScale.z );
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Run" ) );
+		}
+		else
+		{
+			normalizedHorizontalSpeed = 0;
+
+			if( _controller.isGrounded )
+				_animator.Play( Animator.StringToHash( "Idle" ) );
+		}
+
+
+		// we can only jump whilst grounded
+		{
+			_velocity.y = Mathf.Sqrt( 2f * jumpHeight * -gravity );
+			_animator.Play( Animator.StringToHash( "Jump" ) );
+            _sound.PlayJumpSound();
+		}
+
+
+		// apply horizontal speed smoothing it. dont really do this with Lerp. Use SmoothDamp or something that provides more control
+		var smoothedMovementFactor = _controller.isGrounded ? groundDamping : inAirDamping; // how fast do we change direction?
+		_velocity.x = Mathf.Lerp( _velocity.x, normalizedHorizontalSpeed * runSpeed, Time.deltaTime * smoothedMovementFactor );
+
+		// apply gravity before moving
+		_velocity.y += gravity * Time.deltaTime;
+
+		// if holding down bump up our movement amount and turn off one way platform detection for a frame.
+		// this lets us jump down through one way platforms
+		{
+			_velocity.y *= 3f;
+			_controller.ignoreOneWayPlatformsThisFrame = true;
+		}
+
+		_controller.move( _velocity * Time.deltaTime );
+
+		// grab our current _velocity to use as a base for all calculations
+		_velocity = _controller.velocity;
+	}
+
 }
