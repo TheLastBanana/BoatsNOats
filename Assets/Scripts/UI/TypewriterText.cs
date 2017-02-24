3dc66bb243290f276bf71302eb1db59d8db6482b
Braedy Kuzma <puckmaster_84@hotmail.com>
2017-02-23 19:39:33 -0700

    Dialogue system at a good base. Need to implement speeds for typing now.

--- Assets/Scripts/UI/TypewriterText.cs
+++ Assets/Scripts/UI/TypewriterText.cs
@@ -4,18 +4,25 @@ using System.Xml;
 using UnityEngine;
 using UnityEngine.UI;
 
-class TagDef
+interface Tag
+{
+    int start { get; set; }
+    int end { get; set; }
+    string ToString();
+}
+
+class FormatTag : Tag
 {
     public string name { get; set; }
     public int start { get; set; }
     public int end { get; set; }
     public Dictionary<string, string> tagParams {get; private set; }
 
-    public TagDef(string text, int start, int end)
+    public FormatTag(string tagString, int start, int end)
     {
         // Get just the tag name and parse out the parameters
         tagParams = new Dictionary<string, string>();
-        string[] tagValues = text.Split(' ');
+        string[] tagValues = tagString.Split(' ');
         Debug.Assert(tagValues.Length >= 1, "Tag string doesn't have start?");
         this.name = tagValues[0];
 
@@ -33,23 +40,84 @@ class TagDef
 
     public override string ToString()
     {
-        string ret = "TagDef(" + name + ", [";
+        string ret = "FormatTag(" + name + ", [";
         foreach (KeyValuePair<string, string> pair in tagParams)
             ret += pair.Key + "=" + pair.Value + " ";
         ret += "], " + start + ", " + end + ")";
         return ret ;
     }
+
+    public virtual string startTag
+    {
+        get
+        {
+            string[] joinedParams = new string[tagParams.Count];
+            int i = 0;
+            foreach (KeyValuePair<string, string> pair in tagParams)
+            {
+                joinedParams[i] = pair.Key + "=" + pair.Value;
+                ++i;
+            }
+            string ret = name + string.Join(" ", joinedParams);
+            Debug.Log("Format tag start" + ret);
+            return name + string.Join(" ", joinedParams);
+        }
+    }
+
+    public virtual string endTag
+    {
+        get { return name; }
+    }
+}
+
+class SpeedTag : Tag
+{
+    public int start { get; set; }
+    public int end { get; set; }
+    public int speed { get; set; }
+
+    public SpeedTag(int speed, int start, int end)
+    {
+        this.speed = speed;
+        this.start = start;
+        this.end = end;
+    }
+
+    public override string ToString()
+    {
+        return "SpeedTag(" + speed + ", " + start + ", " + end + ")";
+    }
+}
+
+// The color tag ends with something just called color
+class ColorTag : FormatTag
+{
+    // Init exactly the same
+    public ColorTag(string tagString, int start, int end) : base(tagString, start, end) { }
+
+    public override string endTag
+    {
+        get
+        {
+            return "color";
+        }
+    }
 }
 
 class Dialog
 {
-    public string text { get; private set; }
-    public List<TagDef> format { get; private set; }
+    public string text { get; set; }
+    public List<FormatTag> format { get; private set; }
 
-    public Dialog(string text, List<TagDef> format)
+    public Dialog()
     {
-        this.text = text;
-        this.format = format;
+        text = "";
+        format = new List<FormatTag>();
+    }
+
+    public void addTag(FormatTag tag)
+    {
+        format.Add(tag);
     }
 }
 
@@ -65,59 +133,108 @@ public class TypewriterText : MonoBehaviour {
     {
         // Get the text box for later use
         text = GetComponent<Text>();
+        if (text != null)
+            parseFile();
+	}
 
-        // Get the XML document
-        XmlDocument doc = new XmlDocument();
-        doc.LoadXml(dialogFile.text);
+    private Tag addTag(ref Dialog dia, string tagString, int start, int end)
+    {
+        Tag t;
+        // Default tags can just be handled by a tag def
+        if (!tagString.StartsWith("custom"))
+        {
+            // Create the format tag, 
+            FormatTag ft = new FormatTag(tagString, start, end);
+            dia.addTag(ft);
+            t = ft;
+        }
+        else
+        {
+            string[] split = tagString.Split(' ');
+            Debug.Assert(split.Length > 1, "Custom tag had no extra values: " + tagString);
+            
+            // It's a color tag
+            if (split[1].StartsWith("color"))
+            {
+                ColorTag ft = new ColorTag(split[1], start, end);
+                dia.addTag(ft);
+                t = ft;
+            }
+            // It's a speed tag
+            else if (split[1].StartsWith("speed"))
+            {
+                string[] speedSplit = split[1].Split('=');
+                Debug.Assert(speedSplit.Length == 2, "Speed split not 2");
+
+                Debug.Log("Bringing in int: " + speedSplit[1]);
+                int speed = int.Parse(speedSplit[1].Replace("\"", "")); // Values are quoted..
+                SpeedTag st = new SpeedTag(speed, start, end);
+                t = st;
+            }
+            else
+            {
+                Debug.Assert(false, "Unknown custom tag");
+                t = null;
+            }
+        }
+        return t;
+    }
+     
+
+    void parseFile()
+    {
+        XmlDocument doc = new XmlDocument(); // Get the XML document
+        doc.LoadXml(dialogFile.text);  // Load the text asset into the XML document
+
+        // Select and iterate over the text nodes
         XmlNodeList nodes = doc.SelectNodes("//dialog/text");
         foreach (XmlNode node in nodes)
         {
-            List<TagDef> tags = new List<TagDef>();
-            Stack<string> currentTags = new Stack<string>();
+            Dialog dia = new Dialog();
+            Stack<Tag> currentTags = new Stack<Tag>();
             string line = node.InnerXml;
+            Debug.Log("line: " + line);
             string actualLine = "";
             for (int i = 0; i < line.Length; ++i)
             {
+                //Debug.Log("here " + line[i]);
                 // Tag starting
                 if (line[i] == '<')
                 {
-                    int start = actualLine.Length;
-                    ++i; // Advance past <
                     string tag = ""; // This holds all text inside the tag including parameters
+                    int start = actualLine.Length;
                     bool endTag = false; // Set if this tag starts with </
+
+                    ++i; // Advance past <
+                    
+                    // Is this an end tag?
+                    if (line[i] == '/')
+                    {
+                        endTag = true;
+                        ++i; // Advance past /
+                    }
 
                     // Parse until end of tag
                     while (line[i] != '>')
                     {
-                        if (line[i] == '/')
-                            endTag = true;
-                        else
-                            tag += line[i];
-                        ++i;
+                        tag += line[i];
+                        ++i; // Advance past this character
                     }
 
-                    // This was an end tag, gotta make sure it was the last one on the stack
+                    // This was an end tag, pop the most recent tag and set its end
+                    // We can assume this is ending the most recent tag because
+                    // if it wasn't the xml parser would've failed
                     if (endTag)
                     {
-                        string lastTag = currentTags.Pop();
-                        if (lastTag.Equals(tag))
-                        {
-                            foreach (TagDef def in tags)
-                            {
-                                if (def.name.Equals(tag))
-                                {
-                                    def.end = actualLine.Length;
-                                    break;
-                                }
-                            }
-                        }
+                        Tag lastTag = currentTags.Pop();
+                        lastTag.end = actualLine.Length;
                     }
                     // Start tag, make a tagdef and push onto stack
                     else
                     {
-                        TagDef def = new TagDef(tag, start, -1);
-                        currentTags.Push(def.name);
-                        tags.Add(def); // Default end to -1, we'll update when we find the end tag
+                        Tag t = addTag(ref dia, tag, start, -1); // Default end to -1, we'll update when we find the end tag
+                        Debug.Log("After adding '" + tag + "', " + dia.format.Count);
+                        currentTags.Push(t);
                     }
                 } // End tag parsing
                 // Just text parsing
@@ -125,13 +242,18 @@ public class TypewriterText : MonoBehaviour {
                     actualLine += line[i];
             }
 
+            // Finally, attach the dialogue text
+            dia.text = actualLine;
+            
+            // TODO remove debugs
             Debug.Log("Actual text: " + actualLine);
-            foreach (TagDef tag in tags)
+            foreach (FormatTag tag in dia.format)
                 Debug.Log(tag);
-
-            dialogs.Add(new Dialog(actualLine, tags));
+            
+            // Add to the list of parsed dialogs
+            dialogs.Add(dia);
         }
-	}
+    }
 
     // Set to xml text file
     void setText(TextAsset asset)
@@ -153,8 +275,11 @@ public class TypewriterText : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter) && !runText)
+        if (Input.GetKeyDown(KeyCode.P) && !runText)
+        {
+            Debug.Log("Starting text.");
             runText = true;
+        }
 
         if (runText && !started)
         {
@@ -167,31 +292,31 @@ public class TypewriterText : MonoBehaviour {
     IEnumerator AnimateText()
     {
         string fullLine = dialogs[0].text;
-        List<TagDef> tags = dialogs[0].format;
+        List<FormatTag> tags = dialogs[0].format;
+        Debug.Log("Got " + tags.Count + " tags");
         float delay = .125f;
 
         for (int i = 0; i < fullLine.Length; ++i)
         {
             string line = "";
-            Stack<TagDef> activeTags = new Stack<TagDef>();
+            Stack<FormatTag> activeTags = new Stack<FormatTag>();
             for (int j = 0; j < i; ++j)
             {
                 // Push new starting tags
                 for (int tagNum = 0; tagNum < tags.Count; ++tagNum )
                 {
-                    TagDef tag = tags[tagNum];
+                    FormatTag tag = tags[tagNum];
                     if (tag.start == j)
                     {
-                        line += '<' + tag.name + '>';
+                        line += '<' + tag.startTag + '>';
                         activeTags.Push(tag);
-                        Debug.Log("Pushing tag: " + tag.name);
                     }
                 }
 
-                while (activeTags .Count > 0 && activeTags.Peek().end == j)
+                while (activeTags.Count > 0 && activeTags.Peek().end == j)
                 {
-                    TagDef tag = activeTags.Pop();
-                    line += "</" + tag.name + '>';
+                    FormatTag tag = activeTags.Pop();
+                    line += "</" + tag.endTag + '>';
                 }
 
                 line += fullLine[j];
@@ -199,8 +324,8 @@ public class TypewriterText : MonoBehaviour {
             
             while (activeTags.Count > 0)
             {
-                TagDef tag = activeTags.Pop();
-                line += "</" + tag.name + '>';
+                FormatTag tag = activeTags.Pop();
+                line += "</" + tag.endTag + '>';
             }
 
             text.text = line;
