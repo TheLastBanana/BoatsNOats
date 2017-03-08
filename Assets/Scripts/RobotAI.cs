389fd8e106cf857c94701b251794e40241d6a45d
Mickael Zerihoun <mickael.zerihoun@gmail.com>
2017-03-07 23:58:24 -0700

    Robot AI

---
+++ Assets/Scripts/RobotAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotAI : MonoBehaviour {
+
+    public float speed = 7.0f;
+    private Rigidbody2D rb;
+    private Vector2 direction;
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.right;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(rb.velocity.x == 0 || Mathf.Abs(rb.velocity.x) < 2)
+        {
+            direction.x *= -1;            
+        }       
+    }
+    void FixedUpdate()
+    {     
+            rb.velocity = direction * speed;   
+    }
+
+}
