3f792171b9ed7f5a95453c377cc819ac0377ffab
Mickael Zerihoun <mickael.zerihoun@gmail.com>
2017-03-08 17:51:11 -0700

    Created a robot prefab and added raycasting

--- Assets/Scripts/RobotAI.cs
+++ Assets/Scripts/RobotAI.cs
@@ -7,11 +7,21 @@ public class RobotAI : MonoBehaviour {
     public float speed = 7.0f;
     private Rigidbody2D rb;
     private Vector2 direction;
+    Vector3 downdir;
+    Vector3 rt;
+    float width;
+    float height;
+
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody2D>();
         direction = Vector2.right;
-	}
+        downdir = transform.TransformDirection(Vector3.down);
+        rt = transform.GetChild(0).GetComponent<Collider2D>().bounds.size;
+        width = rt.x;
+        height = rt.y;
+        
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -22,7 +32,15 @@ public class RobotAI : MonoBehaviour {
     }
     void FixedUpdate()
     {     
-            rb.velocity = direction * speed;   
+        rb.velocity = direction * speed;
+        if (!Physics2D.Raycast(transform.position + new Vector3(width/2,0,0), downdir, height) )
+        {
+            //print("Hits");
+            rb.velocity = new Vector2(0,0);
+            direction.x *= -1;
+            rb.velocity = direction * speed;
+
+        }
     }
 
 }
