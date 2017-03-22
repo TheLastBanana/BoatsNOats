3b1d1eaa5bd79b1efc28236e7999a8e029ab2c38
Elliot Colp <thelastbanana@gmail.com>
2017-03-22 16:38:09 -0600

    Split circuitry layer rendering into two level halves

--- Assets/Scripts/Circuitry/CircuitManager.cs
+++ Assets/Scripts/Circuitry/CircuitManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CircuitManager : MonoBehaviour
 {
     public Camera circuitCamera;
+    public GameObject rightBound;
 
     static List<List<Circuit>> groups;
     int circuitLayer = -1;
@@ -46,97 +47,112 @@ public class CircuitManager : MonoBehaviour
     // Update circuit objects' connections
     public void RecalculateGroups()
     {
-        UpdateCamera();
-
-        // Render to the texture
-        var rt = circuitCamera.targetTexture;
-        circuitCamera.Render();
-
-        var w = rt.width;
-        var h = rt.height;
-
-        // Set it as the active texture
-        var oldRT = RenderTexture.active;
-        RenderTexture.active = rt;
-
-        // Copy its pixels
-        var tex = new Texture2D(w, h, TextureFormat.RGB24, false);
-        tex.ReadPixels(new Rect(0, 0, w, h), 0, 0);
-
-        // Reset active RenderTexture
-        RenderTexture.active = oldRT;
-
-        // Convert color pixel data into an int array corresponding to group IDs
-        var pixels = tex.GetPixels();
-        var groupIDs = new int[w * h];
-        for (int x = 0; x < w; ++x)
-        {
-            for (int y = 0; y < h; ++y)
-            {
-                var index = y * w + x;
-
-                // The threshold shader has already been applied, so if r is 0, this is black.
-                // Otherise, it's an area we'll want to flood fill later.
-                groupIDs[index] = pixels[index].r == 0 ? 0 : -1;
-            }
-        }
-
-        // Now we want to group areas of the circuit. To do that, we flood fill white areas with a color,
-        // increasing the R value by 1 each time we do a flood fill. The result is that each group of white
-        // pixels (i.e. a connected circuit) has an ID (its R value). Flood fills start at circuit positions,
-        // because these are the only groups that can be powered.
         int curId = 1;
-        foreach (var circuit in FindObjectsOfType<Circuit>()) {
-            var texPos = GetCircuitPixelPosition(circuit.gameObject);
-            if (texPos == null) continue;
+        groups = new List<List<Circuit>>();
 
-            var x = (int)texPos.Value.x;
-            var y = (int)texPos.Value.y;
-
-            // If it's -1, it's an unfilled circuit area
-            if (groupIDs[y * w + x] == -1)
-            {
-                floodFillIntMap(groupIDs, w, h, x, y, curId);
-                ++curId;
-            }
-        }
-
-        int numGroups = curId - 1;
-        if (numGroups > 0)
+        // We run this twice: once for "left" (real world) and once for "right" (alt world)
+        for (int run = 0; run < 2; ++run)
         {
-            groups = new List<List<Circuit>>();
-            for (int i = 0; i < numGroups; ++i)
+            bool side = run == 0 ? true : false;
+            UpdateCamera(side);
+
+            // Render to the texture
+            var rt = circuitCamera.targetTexture;
+            circuitCamera.Render();
+
+            var w = rt.width;
+            var h = rt.height;
+
+            // Set it as the active texture
+            var oldRT = RenderTexture.active;
+            RenderTexture.active = rt;
+
+            // Copy its pixels
+            var tex = new Texture2D(w, h, TextureFormat.RGB24, false);
+            tex.ReadPixels(new Rect(0, 0, w, h), 0, 0);
+
+            // Reset active RenderTexture
+            RenderTexture.active = oldRT;
+
+            // Convert color pixel data into an int array corresponding to group IDs
+            var pixels = tex.GetPixels();
+            var groupIDs = new int[w * h];
+            for (int x = 0; x < w; ++x)
             {
-                groups.Add(new List<Circuit>());
+                for (int y = 0; y < h; ++y)
+                {
+                    var index = y * w + x;
+
+                    // The threshold shader has already been applied, so if r is 0, this is black.
+                    // Otherise, it's an area we'll want to flood fill later.
+                    groupIDs[index] = pixels[index].r == 0 ? 0 : -1;
+                }
             }
 
-            // Assign circuit objects to circuit groups
-            var circuits = FindObjectsOfType<Circuit>();
-            foreach (var circuit in circuits)
+            // Now we want to group areas of the circuit. To do that, we flood fill white areas with a color,
+            // increasing the R value by 1 each time we do a flood fill. The result is that each group of white
+            // pixels (i.e. a connected circuit) has an ID (its R value). Flood fills start at circuit positions,
+            // because these are the only groups that can be powered.
+            int numGroups = 0;
+            foreach (var circuit in FindObjectsOfType<Circuit>())
             {
-                int groupId = 0;
+                // Make sure the object is on the correct side
+                if ((circuit.transform.position.x < rightBound.transform.position.x) != side) continue;
+
                 var texPos = GetCircuitPixelPosition(circuit.gameObject);
+                if (texPos == null) continue;
 
-                // If no valid position, this circuit's group should be left as 0
-                if (texPos != null)
+                var x = (int)texPos.Value.x;
+                var y = (int)texPos.Value.y;
+
+                // If it's -1, it's an unfilled circuit area
+                if (groupIDs[y * w + x] == -1)
                 {
-                    // Find the R value at this pixel and convert it to an integer to get the circuit ID
-                    groupId = groupIDs[(int) texPos.Value.y * w + (int) texPos.Value.x];
-
-                    // Add to the group; otherwise, group will be 0 and the circuit can't be powered
-                    if (groupId != 0)
-                        groups[groupId - 1].Add(circuit);
+                    floodFillIntMap(groupIDs, w, h, x, y, curId);
+                    ++curId;
+                    ++numGroups;
+                }
+            }
+            
+            if (numGroups > 0)
+            {
+                for (int i = 0; i < numGroups; ++i)
+                {
+                    groups.Add(new List<Circuit>());
                 }
 
-                // No power to ungrouped (i.e. broken) circuit pieces
-                if (groupId == 0) circuit.powered = false;
+                // Assign circuit objects to circuit groups
+                var circuits = FindObjectsOfType<Circuit>();
+                foreach (var circuit in circuits)
+                {
+                    // Make sure the object is on the correct side
+                    if ((circuit.transform.position.x < rightBound.transform.position.x) != side) continue;
 
-                var powerSource = circuit.GetComponent<PowerSource>();
-                if (powerSource != null) powerSource.groupId = groupId;
+                    int groupId = 0;
+                    var texPos = GetCircuitPixelPosition(circuit.gameObject);
+
+                    // If no valid position, this circuit's group should be left as 0
+                    if (texPos != null)
+                    {
+                        // Find the R value at this pixel and convert it to an integer to get the circuit ID
+                        groupId = groupIDs[(int)texPos.Value.y * w + (int)texPos.Value.x];
+
+                        // Add to the group; otherwise, group will be 0 and the circuit can't be powered
+                        if (groupId != 0)
+                            groups[groupId - 1].Add(circuit);
+                    }
+
+                    // No power to ungrouped (i.e. broken) circuit pieces
+                    if (groupId == 0) circuit.powered = false;
+
+                    var powerSource = circuit.GetComponent<PowerSource>();
+                    if (powerSource != null) powerSource.groupId = groupId;
+                }
+
+                for (int i = 0; i < numGroups; ++i)
+                    RecalculatePower(i + 1);
+
             }
-
-            for (int i = 0; i < numGroups; ++i)
-                RecalculatePower(i + 1);
         }
     }
 
@@ -179,8 +195,8 @@ public class CircuitManager : MonoBehaviour
         return texPos;
     }
 
-    // Change camera settings to include all circuitry
-    void UpdateCamera()
+    // Change camera settings to include all circuitry on a given side
+    void UpdateCamera(bool left)
     {
         // Get bounds of all GameObjects in circuit layer
         var objects = FindObjectsOfType<GameObject>();
@@ -189,6 +205,9 @@ public class CircuitManager : MonoBehaviour
 
         foreach (GameObject obj in objects)
         {
+            // Make sure the object is on the correct side
+            if ((obj.transform.position.x < rightBound.transform.position.x) != left) continue;
+
             if (obj.layer == circuitLayer)
             {
                 Bounds objBounds = obj.GetComponent<Renderer>().bounds;
