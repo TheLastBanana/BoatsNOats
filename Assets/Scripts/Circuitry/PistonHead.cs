53e7489fe8d0f055b34b13e73aab4bd18b8a710a
Braedy Kuzma <braedy@ualberta.ca>
2017-03-29 00:13:30 -0600

    Pistons apply speed at an angle.

--- Assets/Scripts/Circuitry/PistonHead.cs
+++ Assets/Scripts/Circuitry/PistonHead.cs
@@ -9,15 +9,40 @@ public class PistonHead : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        // If the parent object is null we've been split, we can just destroy this
+        if (parent == null)
+        {
+            Destroy(this);
+            return;
+        }
         piston = parent.GetComponent<Piston>();
 	}
 	
     void OnCollisionStay2D(Collision2D coll)
     {
+        // If the piston object is null we've been split, we can just destroy this
+        if (piston == null)
+        {
+            Destroy(this);
+            return;
+        }
+        
         if (coll.gameObject.tag == "Player" && piston.IsMovingUp())
         {
             Transform t = coll.gameObject.transform;
-            t.position = new Vector3(t.position.x, t.position.y + 2*piston.GetSpeed(), t.position.z);
+            // This is the rotation of the piston
+            Quaternion pistRot = piston.transform.rotation;
+
+            // This is the speed to push Gemma at if we were going straight up
+            Vector3 speed = new Vector3(0, piston.GetSpeed(), 0);
+
+            // This is the speed rotated to push Gemma in the correct direction
+            Vector3 rotSpeed = pistRot * speed;
+
+            Debug.Log(piston.transform.localScale);
+
+            // Add to Gemma's position
+            t.position += rotSpeed;
         }
     }
 }
