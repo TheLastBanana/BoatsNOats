d5895806e753a46a091b7d12b94e495fab73470d
Elliot Colp <thelastbanana@gmail.com>
2017-04-02 23:06:51 -0600

    Add Al and an idle animation

---
+++ Assets/AlignParticleRotation.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AlignParticleRotation : MonoBehaviour {
+    ParticleSystem system;
+    ParticleSystem.MainModule sysMain;
+    public Transform align;
+
+    void Awake()
+    {
+        system = GetComponent<ParticleSystem>();
+        sysMain = system.main;
+    }
+    
+    void Update()
+    {
+        sysMain.startRotationZMultiplier = (-align.localRotation.eulerAngles.z) * Mathf.Deg2Rad;
+	}
+}
