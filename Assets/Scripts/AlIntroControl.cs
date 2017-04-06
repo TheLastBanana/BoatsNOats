5d5906fc1ec5dec0b37e58a72255057f7571e9d7
Elliot Colp <thelastbanana@gmail.com>
2017-04-06 17:22:00 -0600

    Some work on Al's intro cutscene animations

---
+++ Assets/Scripts/AlIntroControl.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class AlIntroControl : MonoBehaviour
+{
+    public ParticleSystem sparks;
+    ParticleSystem.EmissionModule em;
+
+    void Awake()
+    {
+        em = sparks.emission;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GetComponent<Animator>().SetTrigger("Bonk");
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            GetComponent<Animator>().SetTrigger("Get Up");
+        }
+    }
+
+    public void EnableSparks()
+    {
+        em.enabled = true;
+    }
+
+    public void DisableSparks()
+    {
+        em.enabled = false;
+    }
+}
