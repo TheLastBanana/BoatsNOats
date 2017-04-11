227c07011ccaab23bdca7862646a8bd28db230c1
Stuart Bildfell <bildfells760@gmail.com>
2017-04-11 16:15:11 -0600

    Dialogue pointer fix for moving camera

--- Assets/Scripts/UI/DialoguePointer.cs
+++ Assets/Scripts/UI/DialoguePointer.cs
@@ -18,6 +18,7 @@ public class DialoguePointer : MonoBehaviour {
     private Transform pointToTransform;
     private float y;
     private float z;
+    private Transform dummy;
 
 	// Use this for initialization
 	void Start () {
@@ -29,16 +30,16 @@ public class DialoguePointer : MonoBehaviour {
 
         y = objTransform.localPosition.y;
         z = objTransform.localPosition.z;
-
-        objTransform.localPosition = new Vector3(0f, y, z);
-        leftX = objTransform.position.x + leftBound;
-        rightX = objTransform.position.x + rightBound;
+        dummy = new GameObject().GetComponent<Transform>();
+        dummy.parent = objTransform.parent;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (pointTo == null)
             return;
+
+        changeXFromCameraPosition();
 
         // Match X position of object we're pointing to, within bounds
         float newX = pointToTransform.position.x;
@@ -58,4 +59,12 @@ public class DialoguePointer : MonoBehaviour {
         Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
         objTransform.rotation = Quaternion.RotateTowards(objTransform.rotation, q, Time.deltaTime * 1000);
 	}
+
+    private void changeXFromCameraPosition()
+    {
+        dummy.localPosition = new Vector3(0f + leftBound, y, z);
+        leftX = dummy.position.x;
+        dummy.localPosition = new Vector3(0f + rightBound, y, z);
+        rightX = dummy.position.x;
+    }
 }
